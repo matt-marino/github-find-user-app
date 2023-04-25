75f57a76a9632f2a933f52403ef72068040d8a51
Matt Marino <matt.marino@shopify.com>
2023-04-24 22:29:37 -0400

    test

--- github-find-user-app/src/pages/NotFound.jsx
+++ github-find-user-app/src/pages/NotFound.jsx
@@ -9,7 +9,7 @@ function NotFound() {
                 <h1 className='text-8xl font-bold mb-8'>Oops!</h1>
                 <p className='text-5xl mb-8'>404 - Page Not Found!</p>
                 <Link className='btn btn-primary btn-lg' to='/'>
-                <FaHome className='mr-2' />
+                <FaHome className='mr-2 mb-1' />
                 Back To Home
                 </Link>
             </div>
